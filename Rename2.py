dded42c5591c9e88d0bd0d39e6dd9121145c8a46
Vaibhav_Sharma <coolvaibhavfzd@gmail.com>
2023-09-29 13:58:05 +0530

    This is my third commit

--- Rename2.py
+++ Rename2.py
@@ -1,3 +1,7 @@
+import os
+
+directory = "C:\\Users\\user\\OneDrive\\Desktop\\Informatica param automation\\Input\\MDM_INBOUND_PROD\\ECM2"
+
 variable_name = "$$IPC_FILE_NAME_PREFIX"
 new_value = "CUSTOMER_LGLSFDCGLI_DELTA_"
 
@@ -7,23 +11,26 @@ new_value_2 = "CUSTOMER_LGLSFDCGLI_DELTA_DDMMYYYY.csv"
 variable_name_3 = "$$IPC_REJ_FILE_NAME"
 new_value_3 = "CUSTOMER_LGLSFDCGLI_DDMMYYYY_REJ.csv"
 
-file_path =  "C:\\Users\\user\\OneDrive\\Desktop\\Renaming_file_using_python\\PRM_ECM2_FACT_Load.prm"
+# file_path =  "C:\\Users\\user\\OneDrive\\Desktop\\Renaming_file_using_python\\PRM_ECM2_FACT_Load.prm"
 
-with open(file_path, 'r') as file:
-    lines = file.readlines()
+for filename in os.listdir(directory):
+    if filename.endswith(".prm"):
+        file_path = os.path.join(directory, filename)
 
-for i, line in enumerate(lines):
-    if line.startswith(variable_name):
-        lines[i] = new_value
-        lines[i] = f"{variable_name} = {new_value}\n"
+        with open(file_path, 'r') as file:
+            lines = file.readlines()
 
+        for i, line in enumerate(lines):
+            if line.startswith(variable_name):
+                lines[i] = new_value
+                lines[i] = f"{variable_name} = {new_value}\n"
 
-new_line = f"{variable_name2} = {new_value_2}"
-lines.append(new_line)
-new_line2 = f"{variable_name_3} = {new_value_3}"
-lines.append(new_line2)
+        new_line = f"{variable_name2} = {new_value_2}"
+        lines.append(new_line)
+        new_line2 = f"\n{variable_name_3} = {new_value_3}"
+        lines.append(new_line2)
 
-with open(file_path, 'w') as file:
-    file.writelines(lines)
+        with open(file_path, 'w') as file:
+            file.writelines(lines)
 
-file.close()
+        file.close()
